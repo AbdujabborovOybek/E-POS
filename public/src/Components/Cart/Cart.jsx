d6cdeedac59297aff024605f30a9916aaf090946
OybekAbduljabbor <abdujabborovoybek@gmail.com>
2023-08-13 19:57:17 +0500

    fix

--- public/src/Components/Cart/Cart.jsx
+++ public/src/Components/Cart/Cart.jsx
@@ -6,9 +6,43 @@ import { AiOutlineHome, AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 import { BsTrash3 } from "react-icons/bs";
 import { NumericFormat } from "react-number-format";
 import { FaHandHoldingMedical } from "react-icons/fa";
+import { data } from "./data";
 
 export const Cart = memo(() => {
   const [modal, setModal] = useState(false);
+  const [products, setProducts] = useState(data);
+  const [discount, setDiscount] = useState(0);
+  const total = products?.reduce((a, i) => a + i.price * i.count, 0);
+
+  const handlePlus = (id) => {
+    const newProducts = products.map((item) => {
+      if (item.id === id) {
+        return { ...item, count: item.count + 1 };
+      }
+      return item;
+    });
+    setProducts(newProducts);
+  };
+
+  const handleMinus = (id) => {
+    const newProducts = products.map((item) => {
+      if (item.id === id) {
+        if (item.count === 1) {
+          return item;
+        }
+
+        return { ...item, count: item.count - 1 };
+      }
+      return item;
+    });
+    setProducts(newProducts);
+  };
+
+  const handleDelete = (id) => {
+    const newProducts = products.filter((item) => item.id !== id);
+    setProducts(newProducts);
+  };
+
   return (
     <Fragment>
       <div className="cart">
@@ -28,282 +62,41 @@ export const Cart = memo(() => {
         </div>
 
         <div className="cart_body">
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
+          <img
+            className={!products.length ? "empty_card active" : "empty_card"}
+            src={require("./icon.svg").default}
+            alt="empty_cart"
+          />
 
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
+          {products?.map((item) => {
+            return (
+              <div className="cart_item" key={item.id}>
+                <h1>{item.name}</h1>
+                <div className="cart_item_action">
+                  <h1>
+                    <span>
+                      {item.price} * {item.count} ={" "}
+                    </span>
+                    <i>{item.price * item.count}</i>
+                  </h1>
+
+                  <div className="cart_item_btn">
+                    <button onClick={() => handleMinus(item.id)}>
+                      <AiOutlineMinus />
+                    </button>
+                    <span>{item.count}</span>
+                    <button onClick={() => handlePlus(item.id)}>
+                      <AiOutlinePlus />
+                    </button>
+                  </div>
+
+                  <button onClick={() => handleDelete(item.id)}>
+                    <BsTrash3 />
+                  </button>
+                </div>
               </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
-          <div className="cart_item">
-            <h1>Lorem ipsum dolor sit amet consectetur.</h1>
-            <div className="cart_item_action">
-              <h1>
-                <span>120 000 * 3 = </span>
-                <i>1 360 000</i>
-              </h1>
-
-              <div className="cart_item_btn">
-                <button>
-                  <AiOutlineMinus />
-                </button>
-                <span>1</span>
-                <button>
-                  <AiOutlinePlus />
-                </button>
-              </div>
-
-              <button>
-                <BsTrash3 />
-              </button>
-            </div>
-          </div>
+            );
+          })}
         </div>
 
         <div className="cart_total">
@@ -311,7 +104,15 @@ export const Cart = memo(() => {
             <div>
               <h1>
                 <span>Items:</span>
-                <i>10</i>
+
+                <NumericFormat
+                  allowLeadingZeros
+                  thousandSeparator=" "
+                  placeholder="Discount"
+                  suffix="x products"
+                  value={products.length}
+                  displayType="text"
+                />
               </h1>
 
               <NumericFormat
@@ -319,18 +120,38 @@ export const Cart = memo(() => {
                 thousandSeparator=" "
                 placeholder="Discount"
                 suffix=" so'm"
+                value={discount}
+                onValueChange={(values) => {
+                  const { value } = values;
+                  setDiscount(value);
+                }}
               />
             </div>
 
             <div>
               <h1>
                 <span>Total:</span>
-                <i>10 000</i>
+
+                <NumericFormat
+                  allowLeadingZeros
+                  thousandSeparator=" "
+                  placeholder="Discount"
+                  suffix=" so'm"
+                  value={total}
+                  displayType="text"
+                />
               </h1>
 
               <h1>
                 <span>Payable:</span>
-                <i>10 000</i>
+                <NumericFormat
+                  allowLeadingZeros
+                  thousandSeparator=" "
+                  placeholder="Discount"
+                  suffix=" so'm"
+                  value={total - discount}
+                  displayType="text"
+                />
               </h1>
             </div>
           </div>
---
+++ public/src/Components/Cart/data.js
@@ -0,0 +1,51 @@
+export const data = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 235000,
+    count: 2,
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 10000,
+    count: 1,
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 99000,
+    count: 5,
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    price: 22400,
+
+    count: 3,
+  },
+  {
+    id: 5,
+    name: "Product 5",
+    price: 121000,
+    count: 1,
+  },
+  {
+    id: 6,
+    name: "Product 6",
+    price: 45000,
+    count: 1,
+  },
+  {
+    id: 7,
+    name: "Product 7",
+    price: 19000,
+    count: 2,
+  },
+  {
+    id: 8,
+    name: "Product 8",
+    price: 11000,
+    count: 1,
+  },
+];
